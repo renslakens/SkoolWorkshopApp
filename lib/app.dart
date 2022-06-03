e077e22a09d850ff99a446c7952758c503d799a5
Ayrianna <aechatlein@gmail.com>
2022-06-03 20:42:24 +0200

    Added theme to app

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:skoolworkshop/notification.dart';
+import 'package:skoolworkshop/theme.dart';
 
 import 'home.dart';
 import 'api_test.dart';
@@ -12,6 +13,7 @@ class SkoolWorkshopApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SkoolWorkshop',
+      theme: customLightTheme(),
       initialRoute: '/login',  // set back to login after running
       routes: {
         '/api_test' : (BuildContext context) => MyApp(),
