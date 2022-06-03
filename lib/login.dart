b16f3ab597670ff78c1e869e698ba44b891b428a
Rens Lakens <rens@lakens.org>
2022-06-03 14:01:40 +0200

    added bottom navbar

--- lib/login.dart
+++ lib/login.dart
@@ -44,8 +44,6 @@ class _LoginPageState extends State<LoginPage> {
                       color: Colors.black,
                   ),
                   focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: mainColor)),
-
-
                 )),
             const SizedBox(height: 12.0),
             TextField(
