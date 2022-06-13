9292f9b0eb241fa12b3c05348856a8e5ba8376a3
Bananakingo <vandenbosch2003@gmail.com>
2022-06-13 15:35:10 +0200

    merge branch

--- lib/apis.dart
+++ lib/apis.dart
@@ -2,5 +2,10 @@ class apis {
   static String baseUrl = 'http://145.49.7.132:3000';
   static String login = '/api/auth/login';
   static String getUsers = '/api/users';
-  static String getUnAcUsers = '/api/users?isAccepted=0';
+  static String getUnAcUsers = '/api/user?isAccepted=0';
+  static String usersEndpoint = '/user';
+  static String fileUpload = '/api/';
+  static String acceptedProfiles = '/api/user?isAccepted=1';
+  static String deleteProfile = '/api/user/';
+  static String jobs = '/api/job/';
 }
