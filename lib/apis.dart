d6b845a9ea07bc87e796f99334a832b7eb29fdce
Bananakingo <vandenbosch2003@gmail.com>
2022-06-14 14:12:05 +0200

    functioning notification.dart page :)

--- lib/apis.dart
+++ lib/apis.dart
@@ -4,6 +4,11 @@ class apis {
   static String usersEndpoint = '/user';
   static String fileUpload = '/api/';
   static String acceptedProfiles = '/api/user?isAccepted=1';
+
+  static String acceptDocent = '/api/docent/';
+  static String unAcceptedDocent = '/api/Docent?isAccepted=0';
+
+
   static String deleteProfile = '/api/user/';
   static String jobs = '/api/job/';
     static String getWorkshopDetail = '/api/job/';
