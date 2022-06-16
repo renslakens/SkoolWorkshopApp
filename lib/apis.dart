465bce4449f76df93b630e24635be993675593e4
Bananakingo <vandenbosch2003@gmail.com>
2022-06-16 11:36:29 +0200

    updated api links

--- lib/apis.dart
+++ lib/apis.dart
@@ -1,17 +1,17 @@
 class apis {
-  static String baseUrl = 'http://145.49.24.197:3000';
+  static String baseUrl = 'http://145.49.25.100:3000';
   static String login = '/api/auth/login';
   static String usersEndpoint = '/user';
   static String fileUpload = '/api/';
-  static String acceptedProfiles = '/api/Docent?isAccepted=1';
+  static String acceptedProfiles = '/api/auth/?isAccepted=1';
 
   static String acceptUser = '/api/auth/';
   static String deleteDocent = '/api/auth/';
-  static String unAcceptedDocent = '/api/Docent?isAccepted=0';
+  static String unAcceptedDocent = '/api/auth?isAccepted=0';
 
   static String deleteProfile = '/api/auth/';
   static String jobs = '/api/job/';
     static String getWorkshopDetail = '/api/job/';
     static String getOpdracht = '/api/job';
-    static String profiles = '/api/docent?isAccepted=1';
+    static String profiles = '/api/auth?isAccepted=1';
 }
