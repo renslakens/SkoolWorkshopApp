4a049c2c91a050b17515903edaf218cec4b33388
Bananakingo <vandenbosch2003@gmail.com>
2022-06-16 10:02:57 +0200

    fully functional notification.dart page. Only link to detailProfile page needed

--- lib/apis.dart
+++ lib/apis.dart
@@ -1,15 +1,15 @@
 class apis {
-  static String baseUrl = 'http://192.168.1.40:3000';
+  static String baseUrl = 'http://145.49.25.100:3000';
   static String login = '/api/auth/login';
   static String usersEndpoint = '/user';
   static String fileUpload = '/api/';
   static String acceptedProfiles = '/api/user?isAccepted=1';
 
   static String acceptDocent = '/api/docent/';
+  static String deleteDocent = '/api/auth/';
   static String unAcceptedDocent = '/api/Docent?isAccepted=0';
 
-
-  static String deleteProfile = '/api/user/';
+  static String deleteProfile = '/api/auth/';
   static String jobs = '/api/job/';
     static String getWorkshopDetail = '/api/job/';
     static String getOpdracht = '/api/job';
